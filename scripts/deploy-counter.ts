07ec1860c82852381842c002ae1cf7e106c369de
Luis Daniel <danielvieras10@gmail.com>
2022-07-13 23:21:20 -0400

    Script

--- scripts/deploy-counter.ts
+++ scripts/deploy-counter.ts
@@ -4,8 +4,8 @@ import { ethers } from "hardhat";
 const deployCounter = async () => {
     const C = await ethers.getContractFactory("Counter");
     const counter = await C.deploy();
-    console.log(counter.address);
     console.log(await counter.count());
+    console.log(counter.address);
     console.log("counter", await counter.views());
 }
 
